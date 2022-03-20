d31fec7be53d41739cf69bc653b13abc6403b836
tomaszolejnik22 <ziemek22@interia.pl>
2022-03-20 10:48:03 +0100

    Add project files

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public string Result(int number)
+        {
+
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+
+            else if (number % 3 == 0 && number % 5 != 0)
+                return "Fizz";
+
+            else if (number % 3 != 0 && number % 5 == 0)
+                return "Buzz";
+
+            else
+                return number.ToString();
+        }
+        
+
+
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Proszę podaj liczbę całkowitą.");
+            while (true)
+            {
+                var number = GetInput();
+                var result = new FizzBuzz();
+                Console.WriteLine(result.Result(number));
+                Console.WriteLine("\nPodaj następną liczbę.\nJeżeli chcesz wyjść wpisz Q");
+            }
+        }
+
+        private static int GetInput()
+        {
+            while (true)
+            {
+                var value = Console.ReadLine();
+                if (value.ToUpper() == "Q")
+                    Environment.Exit(0);
+
+                if (!int.TryParse(value, out int number ))
+                {
+                    Console.WriteLine("\nPodałeś nieprawidłowe dane. Spróbuj ponownie.\nWyjście wciśnij Q");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
